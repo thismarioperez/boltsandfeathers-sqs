41ea178a9b0f4280e80b529d8325ab699f813f30
Mario Perez <mario.perez415@gmail.com>
2017-11-16 00:37:28 -0500

    updates index to include all core modules

--- source/js/core/index.js
+++ source/js/core/index.js
@@ -5,8 +5,16 @@
  * @description Holds the different core modules.
  *
  */
+import dom from './dom';
+import env from './env';
+import log from './log';
+import detect from './detect';
 import * as util from './util';
 
 export {
+  dom,
+  env,
+  log,
+  detect,
   util
 };
