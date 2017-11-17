23e7c192c02138a67fb5d4d8690d8e803ff893a1
Mario Perez <mario.perez415@gmail.com>
2017-11-17 13:57:42 -0500

    adds a test sqscontroller

--- source/js/controller.js
+++ source/js/controller.js
@@ -9,6 +9,11 @@
 import * as core from './core';
 import intro from './controllers/intro';
 
+// controller modules
+function test(element) {
+  core.log(element);
+}
+
 const controller = {
   /**
    *
@@ -18,8 +23,11 @@ const controller = {
    * @description initializes controller modules.
    */
   init() {
+    // Initialize or register controller modules
     intro.init();
-    // emit intro teardown
+    core.util.sqscontroller.register('test', test);
+
+    // Emit events
     core.emitter.emit('app--intro-teardown');
   }
 };
