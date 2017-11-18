4a3021bd802435755252091791e0d04626ddfaa9
Mario Perez <mario.perez415@gmail.com>
2017-11-18 01:32:29 -0500

    adds controller for navigation

---
+++ source/js/controllers/Navigation.js
@@ -0,0 +1,58 @@
+import * as core from '../core';
+
+/**
+ *
+ * @public
+ * @namespace Navigation
+ * @description Toggles navigation screen and state.
+ * @memberof controller
+ *
+ */
+const Navigation = {
+  open() {
+    core.dom.nav.classList.add('is-active');
+    core.dom.html.classList.add('nav-is-active');
+
+    core.log('Navigation: open');
+  },
+
+  close() {
+    core.dom.nav.classList.remove('is-active');
+    core.dom.html.classList.remove('nav-is-active');
+
+    core.log('Navigation: closed');
+  },
+
+  handleClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (core.dom.nav.classList.contains('is-active')) {
+      core.emitter.emit('app--navigation-close');
+    } else {
+      core.emitter.emit('app--navigation-open');
+    }
+
+    core.log('Navigation: menu trigger clicked');
+  },
+
+  bindListeners() {
+    document.querySelector('.js-menu-trigger').addEventListener('click', this.handleClick.bind(this));
+    core.emitter.on('app--navigation-open', this.open.bind(this));
+    core.emitter.on('app--navigation-close', this.close.bind(this));
+  },
+
+  /**
+   *
+   * @public
+   * @method init
+   * @description Kicks off navigation scripts
+   * @memberof controller.Navigation
+   *
+   */
+  init() {
+    this.bindListeners();
+  }
+};
+
+export default Navigation;
