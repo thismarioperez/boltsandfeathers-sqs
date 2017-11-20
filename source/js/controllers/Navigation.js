b55d4f020fa60bc98727ff00fd2c9ab9a67362f3
Mario Perez <mario.perez415@gmail.com>
2017-11-20 00:04:38 -0500

    resets social icons to regular style when navigation is open

--- source/js/controllers/Navigation.js
+++ source/js/controllers/Navigation.js
@@ -2,6 +2,9 @@ import * as core from '../core';
 
 const menuOpen = 'is-menu-open';
 const isActive = 'is-active';
+const navSocialIconsStyle = 'tweak-social-icons-style-regular';
+let currentIconStyle;
+let needsIconStyleReset;
 
 /**
  *
@@ -9,19 +12,78 @@ const isActive = 'is-active';
  * @namespace Navigation
  * @description Toggles navigation screen and state.
  * @memberof controller
+ * @todo Add mutationObserver to run getIconStyle() when style editor changes are made.
  *
  */
 const Navigation = {
+  /**
+   *
+   * @private
+   * @description Determines whether icon styles need to be reset when navigation is opened.
+   * @method getIconStyle
+   * @memberof Navigation
+   */
+  getIconStyle() {
+    const bodyClassnames = Array.from(core.dom.body.classList);
+    let iconClassname = [];
+
+    // determine if a social icon tweak is active
+    bodyClassnames.forEach((cn) => {
+      iconClassname.push(cn.indexOf('tweak-social-icons-style-'));
+    });
+
+    // sort through the body classnames and save the tweak name for later
+    if (iconClassname.length > 0 && iconClassname[0] !== navSocialIconsStyle) {
+      let classNameToCache = bodyClassnames.filter((className) => {
+        return !className.indexOf('tweak-social-icons-style-');
+      });
+
+      currentIconStyle = classNameToCache[0];
+      needsIconStyleReset = true;
+
+      core.log('Navigation: icon style [' + currentIconStyle + '] will be reset on nav open.');
+      return;
+    }
+
+    // social icon tweak is not active, or the target tweak name is already in play
+    needsIconStyleReset = false;
+    return;
+  },
+
+  /**
+   *
+   * @private
+   * @description Logic for opening the navigation
+   * @method open
+   * @memberof Navigation
+   */
   open() {
     core.dom.nav.classList.add(isActive);
     core.dom.html.classList.add(menuOpen);
 
+    if (needsIconStyleReset) {
+      core.dom.body.classList.remove(currentIconStyle);
+      core.dom.body.classList.add(navSocialIconsStyle);
+    }
+
     core.log('Navigation: open');
   },
 
+  /**
+   *
+   * @private
+   * @description Logic for closing the navigation
+   * @method close
+   * @memberof Navigation
+   */
   close() {
     core.dom.nav.classList.remove(isActive);
     core.dom.html.classList.remove(menuOpen);
+
+    if (needsIconStyleReset) {
+      core.dom.body.classList.remove(navSocialIconsStyle);
+      core.dom.body.classList.add(currentIconStyle);
+    }
 
     core.log('Navigation: closed');
   },
@@ -30,10 +92,10 @@ const Navigation = {
     e.preventDefault();
     e.stopPropagation();
 
-    if (core.dom.nav.classList.contains(isActive) && core.dom.html.classList.contains(menuOpen)) {
-      core.emitter.emit('app--navigation-close');
-    } else {
+    if (!(core.dom.nav.classList.contains(isActive) && core.dom.html.classList.contains(menuOpen))) {
       core.emitter.emit('app--navigation-open');
+    } else {
+      core.emitter.emit('app--navigation-close');
     }
 
     core.log('Navigation: menu trigger clicked');
@@ -55,6 +117,7 @@ const Navigation = {
    */
   init() {
     this.bindListeners();
+    this.getIconStyle();
   }
 };
 
