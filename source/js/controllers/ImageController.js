5cf0dfa43d01d1c01e0bf70e578517a01f8802d0
Mario Perez <mario.perez415@gmail.com>
2017-11-21 13:32:56 -0500

    use ImageHandler to control image loading and reloading

--- source/js/controllers/ImageController.js
+++ source/js/controllers/ImageController.js
@@ -1,6 +1,4 @@
-import * as core from '../core';
-import throttle from 'lodash/throttle';
-import debounce from 'lodash/debounce';
+import ImageHandler from '../class/ImageHandler';
 
 /**
  * @public
@@ -8,85 +6,19 @@ import debounce from 'lodash/debounce';
  * @description Lazy loads images if they are in the viewport
  */
 function ImageController (element) {
-  let imagesToLoad,
-    loadedImages;
-  /**
-   * @private
-   * @name getImagesToLoad
-   * @description scans document and returns images
-   *              that have not been loaded
-   */
-  const getImagesToLoad = () => {
-    return Array.from(element.querySelectorAll('img[data-src]:not([data-load="true"])'));
-  };
-
-  /**
-   * @private
-   * @name getLoadedImages
-   * @description scans document and returns images
-   *              that have already been loaded
-   */
-  const getLoadedImages = () => {
-    return Array.from(element.querySelectorAll('img[data-load="true"]'));
-  };
-
-  /**
-   * @private
-   * @name checkImages
-   * @description detects if image is in viewport, and loads it if true.
-   */
-  const initialLoad = () => {
-    imagesToLoad = getImagesToLoad();
-    // stop here if no images exist
-    if (imagesToLoad.length <= 0) {
-      return false;
-    }
-    core.log('ImageController: load queue: ' + imagesToLoad.length);
-    core.util.loadImages(imagesToLoad, core.util.isElementVisible);
-  };
-
-  /**
-   * @private
-   * @name reloadImages
-   * @description reloads images that have already been loaded.
-   *              Use this on window resize
-   */
-  const reloadImages = () => {
-    loadedImages = getLoadedImages();
-    core.log('ImageController: resize queue: ' + loadedImages.length);
-    core.util.loadImages(getLoadedImages());
-  };
-
-  const handleScroll = () => {
-    initialLoad();
-  };
-
-  const throttledScroll = throttle(handleScroll, 180);
-
-  const handleResize = () => {
-    reloadImages();
-  };
-
-  const debouncedResize = debounce(handleResize, 120);
-
-  const bindListeners = () => {
-    window.addEventListener('resize', debouncedResize);
-    window.addEventListener('scroll', throttledScroll);
-  };
+  let handleImages = null;
 
   const init = () => {
-    bindListeners();
-    initialLoad();
+    handleImages = new ImageHandler(element);
   };
 
   const destroy = () => {
-    window.removeEventListener('resize', debouncedResize);
-    window.removeEventListener('scroll', throttledScroll);
+    handleImages.destroy();
+    handleImages = null;
   };
 
   const sync = () => {
-    initialLoad();
-    reloadImages();
+    handleImages.sync();
   };
 
   init();
