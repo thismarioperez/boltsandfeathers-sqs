0d95fde40b071781f57afe3ea95d7068383974ef
Mario Perez <mario.perez415@gmail.com>
2017-11-24 00:04:06 -0500

    adds AnimationHandler class to core for easy reference app-wide

---
+++ source/js/core/anim.js
@@ -0,0 +1,11 @@
+import AnimationHandler from '../class/AnimationHandler';
+
+/**
+ *
+ * @name anim
+ * @memberof core
+ * @description Easy reference of AnimationHandler class using core
+ */
+const anim = AnimationHandler;
+
+export default anim;
