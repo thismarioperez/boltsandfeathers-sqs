39154a71b2bc0f6f34c3f31c5a67dd5f6fcab1d1
Mario Perez <mario.perez415@gmail.com>
2017-12-01 12:09:21 -0500

    defers calculations if scroll position remains the same

--- source/js/class/AnimateEls.js
+++ source/js/class/AnimateEls.js
@@ -6,8 +6,55 @@ class AnimateEls extends AnimationHandler {
     super();
 
     this.elements = elements;
+    this.lastPosition = -1;
+    this.started = false;
 
     this.start();
+  }
+
+  /**
+   *
+   * @instance
+   * @description adds the active class to animated elements
+   * @memberof AnimateEls
+   * @method toggleActive
+   *
+   */
+  toggleActive () {
+    // toggle active class name
+    this.elements.forEach((el) => {
+      if (core.util.isElementVisible(el)) {
+        el.classList.add('is-active');
+
+      } else {
+        el.classList.remove('is-active');
+      }
+    });
+  }
+
+  /**
+   *
+   * @instance
+   * @description Handles calculations for each animation frame
+   * @memberof AnimateEls
+   * @method loop
+   *
+   */
+  loop () {
+    if ( this.started === false ) {
+      this.started = true;
+      this.toggleActive();
+      return false;
+    }
+
+    // Avoid calculations if not needed
+    if ( this.lastPosition === window.pageYOffset ) {
+      return false;
+    }
+
+    // Update last position and run calculations
+    this.lastPosition = window.pageYOffset;
+    this.toggleActive();
   }
 
   /**
@@ -18,23 +65,13 @@ class AnimateEls extends AnimationHandler {
    * @method start
    *
    */
-  start() {
+  start () {
     // First add animation class name
     this.elements.forEach((el) => {
       el.classList.add('js-animate');
     });
     // Call on parent cycle
-    this.go(() => {
-      // toggle active class name
-      this.elements.forEach((el) => {
-        if (core.util.isElementVisible(el)) {
-          el.classList.add('is-active');
-
-        } else {
-          el.classList.remove('is-active');
-        }
-      });
-    });
+    this.go(this.loop.bind(this));
   }
 
   /**
