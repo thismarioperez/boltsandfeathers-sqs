24cd8e468f41362f47114f3561b07bf3b066d6d6
Mario Perez <mario.perez415@gmail.com>
2017-12-02 23:32:42 -0500

    adds utility function to get video properties

--- source/js/core/util.js
+++ source/js/core/util.js
@@ -126,6 +126,38 @@ const getImageRatio = (img) => {
   return 100;
 };
 
+/**
+ * @public
+ * @description Returns data props from video node
+ * @method getPropsFromNode
+ * @memberof util
+ * @param  {element} node the video node
+ */
+const getPropsFromNode = function(node) {
+  const props = {
+    container: node
+  };
+
+  if (node.getAttribute('data-config-url')) {
+    props.url = node.getAttribute('data-config-url');
+  }
+
+  if (node.getAttribute('data-config-playback-speed')) {
+    props.playbackSpeed = node.getAttribute('data-config-playback-speed');
+  }
+
+  if (node.getAttribute('data-config-filter')) {
+    props.filter = node.getAttribute('data-config-filter');
+  }
+
+  if (node.getAttribute('data-config-filter-strength')) {
+    props.filterStrength = node.getAttribute('data-config-filter-strength');
+  }
+
+  return props;
+};
+
+
 /*******************************************************************************
  * Export
  ******************************************************************************/
@@ -135,5 +167,6 @@ export {
   isElementVisible,
   getElementsInView,
   loadImages,
-  getImageRatio
+  getImageRatio,
+  getPropsFromNode
 };
