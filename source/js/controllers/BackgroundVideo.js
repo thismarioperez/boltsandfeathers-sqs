8a9066b2ecda21ff9a7c7a5eca14dfad96c30a66
Mario Perez <mario.perez415@gmail.com>
2017-12-02 23:32:27 -0500

    adds custom implimentation of squarespace background video

---
+++ source/js/controllers/BackgroundVideo.js
@@ -0,0 +1,32 @@
+import { VideoBackground as VideoBackgroundRenderer } from '@squarespace/video-background-rendering';
+import * as core from '../core';
+
+function BackgroundVideo(element, afterInitialize) {
+  const rootNode = element.querySelector('.video-background');
+  const props = core.util.getPropsFromNode(rootNode);
+  let renderer = new VideoBackgroundRenderer(props);
+
+  const handleResize = () => {
+    renderer.scaleVideo();
+  };
+
+  const handleTweak = () => {
+    renderer.destroy();
+    renderer = new VideoBackgroundRenderer(props);
+  };
+
+  if (typeof afterInitialize === 'function') {
+    afterInitialize({
+      handleResize,
+      handleTweak
+    });
+  }
+
+  return {
+    destroy: () => {
+      renderer.destroy();
+    }
+  };
+}
+
+export default BackgroundVideo;
