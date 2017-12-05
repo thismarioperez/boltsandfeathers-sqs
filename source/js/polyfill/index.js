becf213b8e7ef7b8f16ae0eae68634ae40ccbc2f
Mario Perez <mario.perez415@gmail.com>
2017-12-05 12:12:33 -0500

    removes raf/polyfill because most browsers support it natively

--- source/js/polyfill/index.js
+++ source/js/polyfill/index.js
@@ -1,12 +1,5 @@
 /**
  *
- * @description window.requestAnimationFrame & window.cancelAnimationFrame polyfill
- * @see https://github.com/chrisdickinson/raf
- */
-import 'raf/polyfill';
-
-/**
- *
  * @description Hammertime-js 300ms click delay polyfill
  * @see http://hammerjs.github.io/
  */
